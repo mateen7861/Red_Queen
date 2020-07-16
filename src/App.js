1ca8649ddcd54023e0db4ba5eee518eff0b3f65f
Mateen Mustafa <mateenworld7861@gmail.com>
2020-07-16 08:47:44 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RedQueen from "./components/RedQueen";
+function App() {
+  return (
+    <div className="App">
+      <RedQueen></RedQueen>
+    </div>
+  );
+}
+
+export default App;
